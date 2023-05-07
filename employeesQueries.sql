9b8e9d3cae486b86028776d906c86944af814d2f
Agustin Luengo <83189876+agustinluengo@users.noreply.github.com>
2023-05-07 09:23:16 -0300

    Update employeesQueries.sql
    Adding some basic queries and a query that I do not have the solution yet

--- employeesQueries.sql
+++ employeesQueries.sql
@@ -1,9 +1,59 @@
 'Query 1'
-''Show the different average salaries for each title''
+/* null values */
 
-SELECT t.title, AVG(s.salary) AS 
-FROM salaries s
-RIGHT JOIN titles t ON t.emp_no = s.emp_no
-GROUP BY t.title
-ORDER BY ttl_amt DESC
+SELECT count(*)
+FROM employees.employees
+WHERE emp_no IS NULL OR birth_date IS NULL OR first_name IS NULL OR last_name IS NULL OR gender IS NULL OR hire_date IS NULL;
 
+'Query 2'
+/* Count total employees, men employees, women employees*/
+SELECT COUNT(DISTINCT(emp_no)) AS amount_of_employees, 
+(SELECT COUNT(DISTINCT(emp_no)) AS amount_of_employees FROM employees.employees WHERE gender = 'M') AS 'Male', 
+(SELECT COUNT(DISTINCT(emp_no)) AS amount_of_employees FROM employees.employees WHERE gender = 'F') AS 'Female'
+FROM employees.employees;
+
+'Query 3'
+/* max and min hire date*/
+SELECT MAX(hire_date),MIN(hire_date)
+FROM employees.employees;
+
+'Query 4'
+SELECT emp_no, CONCAT(first_name,' ',last_name) AS 'full_name', YEAR(hire_date) AS hiring,
+CASE
+	WHEN YEAR(hire_date) BETWEEN 1985 AND 1990 THEN 3
+	WHEN YEAR(hire_date) BETWEEN 1991 AND 1995 THEN 2
+ELSE 1
+END AS tier
+FROM employees.employees
+/* HAVING tier = 3 */
+ORDER BY tier DESC;
+
+
+
+/* not working yet, I'm trying to have the employees' tier and count amount of ppl within each tier
+
+
+WITH tiers AS 
+(SELECT emp_no, CONCAT(first_name,' ',last_name) AS 'full_name' ,YEAR(hire_date) AS hiring,
+CASE
+	WHEN YEAR(hire_date) BETWEEN 1985 AND 1990 THEN 3
+	WHEN YEAR(hire_date) BETWEEN 1991 AND 1995 THEN 2
+ELSE 1
+END AS tier
+FROM employees.employees
+ORDER BY tier)
+
+SELECT COUNT(tier)
+FROM employees.employees
+GROUP BY tier
+
+*/
+
+/* employees by department */
+
+'Query 6'
+SELECT d2.dept_name, COUNT(d1.emp_no) AS emp_n
+FROM employees.dept_emp d1
+LEFT JOIN employees.departments d2 ON d2.dept_no = d1.dept_no
+GROUP BY d1.dept_no
+ORDER BY emp_n DESC;
